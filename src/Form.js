983c7b517c5ee10e6b5e54d9afdd8f86f4ba22f6
Eugene Dv <vakuka@gmail.com>
2021-09-04 18:45:20 -0400

    Add Edit individual Items

--- src/Form.js
+++ src/Form.js
@@ -1,20 +1,24 @@
-import React, {useState, useContext} from 'react'
+import React, {useContext} from 'react'
 import { TodoContext } from './TodoContext'
 
 const Form = ({ changeTitle, title}) => {
 
-    [title, changeTitle] = useState('')
     const context = useContext(TodoContext)
 
     const onSubmitHandler = (event) => {
         event.preventDefault()
-        context.addTodo(title)
+        context.addTodo()
+    }
+
+    const onChangeHandler = (title) => {
+        context.updateTitle(title)
+
     }
 
     return (
         <div>
             <form onSubmit={(event) => onSubmitHandler(event)}>
-                <input type="text" value={title} onChange={(event) => changeTitle(event.target.value)} />
+                <input type="text" value={context.title} onChange={(event) => onChangeHandler(event.target.value)} />
                 <button>Submit</button>
             </form>
         </div>
--- src/TodoContext.js
+++ src/TodoContext.js
@@ -7,6 +7,9 @@ export const TodoProvider = (props) => {
     const [user, setUser] = useState('Eugene')
     const [todos, setTodo] = useState([])
 
+    const [title, setTitle] = useState('')
+    const [id, setId] = useState(0)
+
     const changeUser = () => {
         setUser(prevUser => {
             return prevUser === 'Mike' ? 'Eugene' : 'Mike';
@@ -38,20 +41,49 @@ export const TodoProvider = (props) => {
         setTodo(newTodos)
     })
 
+    const editTodo = (id)  => {
+        todos.forEach((todo) => {
+           if(todo.id === id) {
+               setTitle(todo.title)
+               setId(todo.id)
+           }
+       })
+    }
 
-    const addTodo = (title) => {
+    const addTodo = () => {
+        if(id > 0){
+            updateTodo()
+            return
+        }
         const newId = todos[todos.length - 1].id + 1;
         const newTodos = [...todos, { id: newId, title: title, completed: false}]
         setTodo(newTodos)
+        resetForm()
     }
-    
 
-    const editTodo = (id, title) => {
-        console.log(id + ' - ' + title)
+    const updateTitle =(title) => {
+        setTitle(title)
+    }
+
+    const updateTodo = () => {
+        const newTodos = [...todos]
+        newTodos.map((todo) => {
+            if(todo.id === id){
+                todo.title = title
+            }
+            return todo
+        })
+        setTodo(newTodos)
+        resetForm()
+    }
+
+    const resetForm = () => {
+        setTitle('')
+        setId(0)
     }
 
     return (
-        <TodoContext.Provider value={{ user, changeUser, todos, completeTodo, deleteTodo, addTodo, editTodo}}>
+        <TodoContext.Provider value={{ user, changeUser, todos, completeTodo, deleteTodo, addTodo, title, editTodo, updateTitle}}>
             {props.children}
         </TodoContext.Provider>
     )
